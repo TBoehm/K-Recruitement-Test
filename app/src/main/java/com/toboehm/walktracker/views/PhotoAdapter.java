b99f5d37f1f4591d51224ea8567ccf4a08617887
Tobias Boehm <contact@toboehm.de>
2015-10-24 20:11:51 +0200

    PhotoAdapter.java added for later use;

---
+++ app/src/main/java/com/toboehm/walktracker/views/PhotoAdapter.java
@@ -0,0 +1,31 @@
+package com.toboehm.walktracker.views;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.toboehm.walktracker.R;
+import com.toboehm.walktracker.network.responsmodel.PPhoto;
+
+/**
+ * Created by Tobias Boehm on 22.09.2015.
+ */
+public class PhotoAdapter extends ArrayAdapter<PPhoto> {
+
+    public PhotoAdapter(final Context context) {
+        super(context, R.layout.support_simple_spinner_dropdown_item);
+    }
+
+    @Override
+    public View getView(final int position, final View convertView, final ViewGroup parent) {
+
+        PPhotoView view = (PPhotoView) convertView;
+        if (view == null) {
+            view = new PPhotoView(getContext());
+        }
+        view.setContent(getItem(position));
+
+        return view;
+    }
+}
